688cf2c456b0e5efb3704be59180bf53a5b5e561
parkag <grzegorz.parka@gmail.com>
2014-12-02 19:25:27 +0100

    sample output checking from external files

--- oj/runner/runners.py
+++ oj/runner/runners.py
@@ -3,50 +3,66 @@ import shlex
 
 class Runner(object):
 
-	def compile(self):
-		raise NotImplementedError
+    def compile(self):
+        raise NotImplementedError
 
-	def run(self):
-		raise NotImplementedError
+    def run(self):
+        raise NotImplementedError
 
-	def clean_up(self):
-		raise NotImplementedError
+    def clean_up(self):
+        raise NotImplementedError
+
 
 
 class CRunner(Runner):
 
-	def __init__(self):
-		self.compile_command = "gcc {0} -o {1}"
-		self.executable_name = "program_c"
+    def __init__(self):
+        self.compile_command = "gcc {0} -o {1}"
+        self.executable_name = "program_c"
 
-	def compile(self, filename):
-		self.filename = filename
-		cmd = self.compile_command.format(self.filename, self.executable_name)
-		subprocess.call(cmd.split())
+    def compile(self, filename):
+        self.filename = filename
+        cmd = self.compile_command.format(self.filename, self.executable_name)
+        subprocess.call(cmd.split())
 
-	def run(self):
-		run_command = './{0}'.format(self.executable_name)
-		program_output = subprocess.check_output(run_command.split(), shell=True)
-		return program_output
+    def run(self):
+        run_command = './{0}'.format(self.executable_name)
+        program_output = subprocess.check_output(run_command.split(), shell=False)
+        return program_output
 
-	def clean_up(self):
-		clean_up_command = "rm {0}".format(self.executable_name)
-		subprocess.call(clean_up_command.split())
+    def clean_up(self):
+        clean_up_command = "rm {0}".format(self.executable_name)
+        subprocess.call(clean_up_command.split())
+
 
 
 class PythonRunner(Runner):
-	
-	def __init__(self):
-		pass
+    
+    def __init__(self):
+        pass
 
-	def compile(self, filename):
-		# no need to compile python code! ;)
-		self.filename = filename
+    def compile(self, filename):
+        # no need to compile python code! ;)
+        self.filename = filename
 
-	def run(self):
-		run_command = 'python {0}'.format(self.filename)
-		program_output = subprocess.check_output(run_command.split())
-		return program_output
+    def run(self):
+        run_command = 'python {0}'.format(self.filename)
+        program_output = subprocess.check_output(run_command.split())
+        return program_output
 
-	def clean_up(self):
-		pass
+    def run_test(self, test_input_fname, test_output):
+        #pipe test_input to program call
+        test_input = open(test_input_fname)
+        run_command = 'python {0}'.format(self.filename)
+        p = subprocess.Popen(run_command.split(), stdin=test_input, stdout=subprocess.PIPE)
+        p.wait()
+        program_output = p.communicate()
+        
+        return program_output[0]
+        #if program_output == test_output:
+        #    return True
+        #else:
+        #    return False
+
+    def clean_up(self):
+        pass
--- test/test_runner.py
+++ test/test_runner.py
@@ -1,40 +1,57 @@
 import unittest
+from textwrap import dedent
 
 from oj.runner.runners import CRunner, PythonRunner
 
+
 class TestRunner(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        c_program = r"""
+        int main(){
+            puts("Hello world!");
+            return 0;
+        }
+        """
+        cls.c_fname = 'tmp/hello.c'
+        with open(cls.c_fname, 'w') as f:
+            f.write(c_program)
 
-	@classmethod
-	def setUpClass(cls):
-		c_program = r"""
-		int main(){
-			puts("Hello world!");
-			return 0;
-		}
-		"""
-		cls.c_fname = 'tmp/hello.c'
-		with open(cls.c_fname, 'w') as f:
-			f.write(c_program)
+        python_program = r"print 'Sss!'"
+        cls.python_fname = 'tmp/hello.py'
+        with open(cls.python_fname, 'w') as f:
+            f.write(python_program)
 
-		python_program = r"print 'Sss!'"
-		cls.python_fname = 'tmp/hello.py'
-		with open(cls.python_fname, 'w') as f:
-			f.write(python_program)
+        python_program_for_io = dedent(
+            """
+            import sys
+            for line in sys.stdin:
+                print 2*int(line)"""
+        )
+        cls.python_fname_io = 'tmp/input.py'
+        with open(cls.python_fname_io, 'w') as f:
+            f.write(python_program_for_io)
 
-	def test_run_c_program(self):
-		runner = CRunner()
-		runner.compile(self.c_fname)
-		output = runner.run()
-		runner.clean_up()
-		self.assertEquals(output, "Hello world!\n")
+    def test_run_c_program(self):
+        runner = CRunner()
+        runner.compile(self.c_fname)
+        output = runner.run()
+        runner.clean_up()
+        self.assertEquals(output, "Hello world!\n")
 
-	def test_run_python_program(self):
-		runner = PythonRunner()
-		runner.compile(self.python_fname)
-		output = runner.run()
-		self.assertEquals(output, "Sss!\n")
+    def test_run_python_program(self):
+        runner = PythonRunner()
+        runner.compile(self.python_fname)
+        output = runner.run()
+        self.assertEquals(output, "Sss!\n")
 
-	@classmethod
-	def tearDownClass(self):
-		# remove tmp dir with stuff
-		pass
+    def test_run_python_test(self):
+        runner = PythonRunner()
+        runner.compile(self.python_fname_io)
+        output = runner.run_test('input.txt', "2\n4\n6\n")
+        self.assertEquals(output, "2\n4\n6\n")
+
+    @classmethod
+    def tearDownClass(self):
+        # remove tmp dir with stuff
+        pass
